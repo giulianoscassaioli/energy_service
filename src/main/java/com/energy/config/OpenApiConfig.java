839d5597e12c269edf871e8cd71e242ecdaab046
giulg <giulg@LAPTOP-B5M0M7UH.station>
2022-03-24 20:57:18 +0100

    swagger comment

--- src/main/java/com/energy/config/OpenApiConfig.java
+++ src/main/java/com/energy/config/OpenApiConfig.java
@@ -8,7 +8,15 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Customer Management Service API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Customer Management Service API", version = "v1"
+,description = "Questo applicativo serve per gestire il fatturato dei clienti iscritti al"
+		+ " serivizio. Le entita coinvolte in questo servizio sono appunto dei Clienti, le loro Fatture, gli Indirizzi "
+		+ " dei clienti, ed una lista di  oltre 7000 Comuni con relative Provincie italiane realmente esistenti. Prima di poter effettuare qualsiasi "
+		+ " operazione andare in fondo alla pagina per iscriversi ed"
+		+ " effettuare il login. Inserire successivamente il Bearer token ricevuto come risposta utilizzando il pulsante"
+		+ " Authorize"
+		+ " Le richieste POST,PUT e DELETE sono disponibili soltanto per gli utenti iscritti col ruolo admin, per gli utenti"
+		+ " con ruolo user sono diponibili soltanto le richieste GET"))
 @SecurityScheme(
 		
 		name = "bearerAuth",
